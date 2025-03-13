343d9b8408bda36be63fb2c024f637bf113558d4
jose-angel gomez-lopez <jose-angel.gomez@sipearl.com>
2025-03-13 16:23:32 +0100

    Update update.go
    Signed-off-by: jose-angel gomez-lopez <jose-angel.gomez@sipearl.com>

--- pkg/update.go
+++ pkg/update.go
@@ -1,12 +1,16 @@
 package magellan
 
 import (
-	"encoding/json"
 	"fmt"
-	"net/http"
+	"io"
 	"net/url"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
 
-	"github.com/OpenCHAMI/magellan/pkg/client"
+	"github.com/stmcginnis/gofish"
+	"github.com/stmcginnis/gofish/redfish"
 )
 
 type UpdateParams struct {
@@ -15,43 +19,126 @@ type UpdateParams struct {
 	FirmwareVersion  string
 	Component        string
 	TransferProtocol string
+	Insecure         bool
+}
+
+// FileCopy copies a single file from src to dst
+func FileCopy(src string, dst string) error {
+
+	sourceFileStat, err := os.Stat(src)
+	if err != nil {
+		return err
+	}
+
+	if !sourceFileStat.Mode().IsRegular() {
+		return fmt.Errorf("%s is not a regular file", src)
+	}
+
+	source, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer source.Close()
+
+	destination, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer destination.Close()
+	foo, err := io.Copy(destination, source)
+	fmt.Println(foo)
+	return err
 }
 
 // UpdateFirmwareRemote() uses 'gofish' to update the firmware of a BMC node.
 // The function expects the firmware URL, firmware version, and component flags to be
 // set from the CLI to perform a firmware update.
+// Example:
+// ./magellan update https://192.168.23.40 --username root --password 0penBmc
+// --firmware-url http://192.168.23.19:1337/obmc-phosphor-image.static.mtd.tar
+// --scheme TFTP
+//
+// being:
+// q.URI https://192.168.23.40
+// q.TransferProtocol TFTP
+// q.FirmwarePath http://192.168.23.19:1337/obmc-phosphor-image.static.mtd.tar
 func UpdateFirmwareRemote(q *UpdateParams) error {
 	// parse URI to set up full address
 	uri, err := url.ParseRequestURI(q.URI)
 	if err != nil {
 		return fmt.Errorf("failed to parse URI: %w", err)
 	}
-	uri.User = url.UserPassword(q.Username, q.Password)
 
-	// set up other vars
-	updateUrl := fmt.Sprintf("%s/redfish/v1/UpdateService/Actions/SimpleUpdate", uri.String())
-	headers := map[string]string{
-		"Content-Type":  "application/json",
-		"cache-control": "no-cache",
+	if !(q.Insecure) {
+		// SECURE MODE
+		fmt.Println("SECURE MODE (OpenBMC mode https://github.com/openbmc)")
+		// first to copy in local the file that we want to uplaod to the BMC
+		currentDir, err := os.Getwd()
+		if err != nil {
+			fmt.Println(err)
+		}
+		destPath := filepath.Join(currentDir, "newfw.mtd.tar")
+		FileCopy(q.FirmwarePath, destPath)
+
+		// On OpenBMC we need to execute this kind of command
+		// 	curl  -k -H "X-Auth-Token: $token"  -H "Content-Type:multipart/form-data" -X POST \
+		//  -F UpdateParameters='{"Targets":["/redfish/v1/Managers/bmc"],"@Redfish.OperationApplyTime":"Immediate"};type=application/json' \
+		//  -F 'UpdateFile=@obmc-phosphor-image-sipearl-evb0-20250304115722.static.mtd.tar;type=application/octet-stream' \
+		// https://root:0penBmc@192.168.75.39/redfish/v1/UpdateService/update
+
+		curlcmd := "curl"
+
+		prefix := "//" + q.Username + ":" + q.Password + "@"
+		theurl := strings.Replace(uri.String(), "//", prefix, 1)
+		theurl += "/redfish/v1/UpdateService/update"
+		cmd := exec.Command(curlcmd, "-k", "-H \"X-Auth-Token: $token\"",
+			"-H \"Content-Type:multipart/form-data\"", "-X", "POST",
+			"-F", "UpdateFile=@newfw.mtd.tar;type=application/octet-stream",
+			"-F", "UpdateParameters={\"Targets\":[\"/redfish/v1/Managers/bmc\"],\"@Redfish.OperationApplyTime\":\"Immediate\"}",
+			theurl)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		err = cmd.Run()
+		if err != nil {
+			fmt.Printf("cmd.Run() failed with %s\n", err)
+		} else {
+			fmt.Println("Firmware update initiated successfully.")
+		}
+		// remove the copy of the BMC image
+		err = os.Remove("newfw.mtd.tar")
+		if err != nil {
+			fmt.Printf("os.Remove failed with %s\n", err)
+		}
+
+		return nil
 	}
-	b := map[string]any{
-		"UpdateComponent":  q.Component, // BMC, BIOS
-		"TransferProtocol": q.TransferProtocol,
-		"ImageURI":         q.FirmwarePath,
-	}
-	data, err := json.Marshal(b)
+
+	// INSECURE MODE
+	// Connect to the Redfish service using gofish
+	client, err := gofish.Connect(gofish.ClientConfig{Endpoint: uri.String(), Username: q.Username, Password: q.Password, Insecure: q.Insecure})
 	if err != nil {
-		return fmt.Errorf("failed to marshal data: %v", err)
+		return fmt.Errorf("failed to connect to Redfish service: %w", err)
 	}
-	res, body, err := client.MakeRequest(nil, updateUrl, "POST", data, headers)
+	defer client.Logout()
+
+	// Retrieve the UpdateService from the Redfish client
+	updateService, err := client.Service.UpdateService()
 	if err != nil {
-		return fmt.Errorf("something went wrong: %v", err)
-	} else if res == nil {
-		return fmt.Errorf("no response returned (url: %s)", updateUrl)
+		return fmt.Errorf("failed to get update service: %w", err)
 	}
-	if len(body) > 0 {
-		fmt.Printf("%d: %v\n", res.StatusCode, string(body))
+
+	// Build the update request payload
+	req := redfish.SimpleUpdateParameters{
+		ImageURI:         q.FirmwarePath,
+		TransferProtocol: redfish.TransferProtocolType(q.TransferProtocol),
 	}
+
+	// Execute the SimpleUpdate action
+	err = updateService.SimpleUpdate(&req)
+	if err != nil {
+		return fmt.Errorf("firmware update failed: %w", err)
+	}
+	fmt.Println("Firmware update initiated successfully.")
 	return nil
 }
 
@@ -61,18 +148,23 @@ func GetUpdateStatus(q *UpdateParams) error {
 	if err != nil {
 		return fmt.Errorf("failed to parse URI: %w", err)
 	}
-	uri.User = url.UserPassword(q.Username, q.Password)
-	updateUrl := fmt.Sprintf("%s/redfish/v1/UpdateService", uri.String())
-	res, body, err := client.MakeRequest(nil, updateUrl, "GET", nil, nil)
+
+	// Connect to the Redfish service using gofish
+	client, err := gofish.Connect(gofish.ClientConfig{Endpoint: uri.String(), Username: q.Username, Password: q.Password, Insecure: q.Insecure})
 	if err != nil {
-		return fmt.Errorf("something went wrong: %v", err)
-	} else if res == nil {
-		return fmt.Errorf("no response returned (url: %s)", updateUrl)
-	} else if res.StatusCode != http.StatusOK {
-		return fmt.Errorf("returned status code %d", res.StatusCode)
+		return fmt.Errorf("failed to connect to Redfish service: %w", err)
 	}
-	if len(body) > 0 {
-		fmt.Printf("%v\n", string(body))
+	defer client.Logout()
+
+	// Retrieve the UpdateService from the Redfish client
+	updateService, err := client.Service.UpdateService()
+	if err != nil {
+		return fmt.Errorf("failed to get update service: %w", err)
 	}
+
+	// Get the update status
+	status := updateService.Status
+	fmt.Printf("Update Status: %v\n", status)
+
 	return nil
 }
